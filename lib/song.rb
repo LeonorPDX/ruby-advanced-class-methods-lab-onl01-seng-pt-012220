325d0946981b12ae1b5eebf3326306ffe19825f9
Leonor Colbert <leonoracolbert@gmail.com>
2020-02-10 20:22:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.find_or_create_by_name(name)
     if self.all.include?(name)
-      Song.find_by_name(name)
+      find_by_name(name)
     else
       create_by_name(name)
     end
